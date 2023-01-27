ca85f95068b893de3254d2b4dbc2b1835656ce55
Hiloliddin <38894285+hiloliddinj@users.noreply.github.com>
2023-01-27 15:34:36 +0500

    ...

---
+++ Assets/Scripts/Constants/AnimeCnst.cs
@@ -0,0 +1,11 @@
+
+
+public class AnimeCnst
+{
+    public const string isShooting = "isShooting";
+    public const string isClimbing = "isClimbing";
+    public const string isDead = "isDead";
+    public const string isFallingEnded = "isFallingEnded";
+    public const string isLedderFinished = "isLedderFinished";
+    public const string blend = "blend";
+}
